0ca38ced03b737dda7b337a10c253d263653be0c
dobam <ckehqja@gmail.com>
2024-05-05 00:59:51 +0900

    feat printSubjectList() 과목출력메서드 추가

--- src/service/SubjectService.java
+++ src/service/SubjectService.java
@@ -4,6 +4,8 @@ import model.Subject;
 import repository.Repository;
 import repository.SubjectRepository;
 
+import java.util.List;
+
 public class SubjectService implements Service<Subject> {
 
     private final Repository<Subject> repository = new SubjectRepository();
@@ -18,5 +20,11 @@ public class SubjectService implements Service<Subject> {
         return repository.findById(id);
     }
 
-
+    public void printSubjectList() {
+        List<Subject> list = repository.getList();
+        for (Subject subject : list) {
+            System.out.print(subject.getId() + ":" + subject.getSubjectName() +  ", ");
+        }
+        System.out.println();
+    }
 }
